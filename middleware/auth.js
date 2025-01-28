20ff4a9dfb859aadc65271c971815691d589113d
hamsoace <hamsoace@gmail.com>
2025-01-28 14:02:01 +0300

    usertype added

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,7 +21,7 @@ const auth = async (req, res, next) => {
     req.user = {
       ...user.toObject(),
       formattedPhone: user.getFormattedPhone('safaricom'),
-      role: user.role,
+      role: user.userType,
     };
     
     next();
